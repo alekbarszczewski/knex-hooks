39249fa593a274399c3f1e2fbcbb9ab30d0e888e
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2019-08-14 16:12:35 +0200

    Solve 'first' query problem

--- src/builder-params.js
+++ src/builder-params.js
@@ -2,7 +2,8 @@
 const methods = ['insert', 'update', 'del', 'select'];
 
 const builderParams = function (builder) {
-  const method = builder._method;
+  let method = builder._method;
+  if (method === 'first') { method = 'select'; }
   if (methods.indexOf(method) === -1) { return null; }
   const table = builder._single.table;
   if (!table) { return null; }
